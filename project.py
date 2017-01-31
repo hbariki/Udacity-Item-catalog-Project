9714fe6f3f16380944f9ab91cf12a9d72af32838
Harshini <harshinibariki@gmail.com>
2017-01-31 05:09:02 -0800

    Made some changes

--- project.py
+++ project.py
@@ -187,7 +187,7 @@ def logout():
         if login_session['provider'] == 'google':
             gdisconnect()
             del login_session['gplus_id']
-            del login_session['credentials.access_token']
+            del login_session['access_token']
         del login_session['username']
         del login_session['email']
         del login_session['picture']
